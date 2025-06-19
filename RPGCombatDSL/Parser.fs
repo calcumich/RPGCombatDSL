72b3e4485657962f15f20de1bbbaf009b05557ce
Charles Cal <145075745+calcumich@users.noreply.github.com>
2025-06-18 21:42:20 -0400

    Add simple parser for combat script

--- RPGCombatDSL/Parser.fs
+++ RPGCombatDSL/Parser.fs
@@ -1,2 +1,23 @@
-﻿module Parser
+module Parser
 
+open System
+open Types
+
+/// Parse a single line of the DSL into a Turn option
+let private parseLine (line: string) : Turn option =
+    let words =
+        line.Split([| ' ' |], StringSplitOptions.RemoveEmptyEntries)
+        |> Array.toList
+
+    match words with
+    | [ actor; "attacks"; target ] ->
+        Some { Actor = actor; Action = Attack target }
+    | [ actor; "defends" ] ->
+        Some { Actor = actor; Action = Defend }
+    | _ -> None
+
+/// Parse a script consisting of one command per line into a list of Turns
+let parseTurns (script: string) : Turn list =
+    script.Split([| '\n'; '\r' |], StringSplitOptions.RemoveEmptyEntries)
+    |> Array.choose parseLine
+    |> Array.toList
